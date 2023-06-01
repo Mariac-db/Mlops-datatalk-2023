6ec97eb064d441ba474e178e5871666ac028129f
Maria Durango <camiladurangob@gmail.com>
2023-05-31 21:54:27 -0500

    homeworks files until week 2

---
+++ cohort-2023/homeworks/week2/preprocess_data.py
@@ -0,0 +1,98 @@
+
+import argparse
+import os
+import pickle
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+
+# to save data variables into pickle
+def dump_pickle(obj, filename):
+    with open(filename, "wb") as f_out: 
+        return pickle.dump(obj, f_out)
+
+# read data
+def read_dataframe(filename: str):
+    df = pd.read_parquet(filename)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+
+    return df
+
+#process data for model input
+def preprocess(df: pd.DataFrame, dv: DictVectorizer, fit_dv: bool = False):
+    df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
+    categorical = ['PU_DO']
+    numerical = ['trip_distance']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+    if fit_dv:
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+    return X, dv
+
+def run(raw_data_path: str, dest_path :str, dataset: str = "green"): 
+
+    # load parquet files
+    df_train = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_tripdata_2022-01.parquet")
+    )
+    df_valid = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_tripdata_2022-02.parquet")
+    )
+    df_test = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_tripdata_2022-03.parquet")
+    )
+
+
+    # extract the target
+    target = 'duration'
+    y_train = df_train[target].values
+    y_valid = df_valid[target].values
+    y_test = df_test[target].values
+
+    # fit the dictvectorizer and preprocess data
+    dv = DictVectorizer()
+    X_train, dv = preprocess(df_train, dv, fit_dv=True)
+    X_valid, _ = preprocess(df_valid, dv, fit_dv=False)
+    X_test, _ = preprocess(df_test, dv, fit_dv=False)
+
+    # create dest_path folder unless it already exists
+    os.makedirs(dest_path, exist_ok=True)
+
+    # save dictvectorizer and datasets
+    dump_pickle(dv, os.path.join(dest_path, "dv.pkl"))
+    dump_pickle((X_train, y_train), os.path.join(dest_path, "train.pkl"))
+    dump_pickle((X_valid, y_valid), os.path.join(dest_path, "valid.pkl"))
+    dump_pickle((X_test, y_test), os.path.join(dest_path, "test.pkl"))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--raw_data_path",
+        help="folder de data raw original"
+    )
+    parser.add_argument(
+        "--dest_path",
+        help="localización donde el path será guardado"
+    )
+    args = parser.parse_args()
+
+    run(args.raw_data_path, args.dest_path)
+
+
+
+# no sabes como correrlo? dont worry
+# con cd posicionate donde está el código data_process.py 
+# haz un data_process.py -help esto de dará los argumentos necesarios
+# notas que en el parser ingresamos dos argumentos qeue son raw data y dest path? pues es lo mismo, para correrlo especifica con sus valores respectivos 
+# tu terminal se verá algo así o en mi caso luce así: (mlops) mdurango$ python preprocess_data.py --raw_data_path data/ --dest_path output/
+# en data está mi raw data porque yo lo pusé ahí, solo indico mi path de salida que se llamará otput :) 
+
+
